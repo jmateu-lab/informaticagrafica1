822705dca8f5a755029d48dab768276288f32a0c
J.Mateu <jmateu.upv@gmail.com>
2026-04-22 09:12:41 +0200

    Update index.js
    Añadiendo POST

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
+let maquina={
+    nombre: "Cinta transportadora",
+    estado: "Encendida",
+    temperatura: 36.5
+};
+
+
+//GET -> CONSULTAMOS EL ESTADO
 app.get('/estado', (req, res) => {
+    res.json(maquina);
+});
+
+//POST -> CAMBIAMOS EL ESTADO
+app.post('/cambiar-estado',(req,res) => {
+    maquina.estado=req.body.estado;
     res.json({
-        maquina: "Cinta transportadora",
-        estado: "ENCENDIDA"
+        mensaje:"Estado actualizado",
+        maquina: maquina
     });
 });
 
